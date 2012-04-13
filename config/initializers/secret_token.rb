1874fc0fd1081505d003b07e38eea4496e5c5c1a
Gunnar Magholder <gmagholder@googlemail.com>
2012-04-13 18:55:03 +0200

    Initial Upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friseur::Application.config.secret_token = '64617102468ad36e7e3a77f315e9bc4512bbfbe046fecc762b8428077f658ecb283864f544fa1f8d958b3f0f9e74dc9c21203047198c97a0efbbc7b65e3ec722'
